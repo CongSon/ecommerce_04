055f672661324cef95d311555d042193adad6714
nguyen chi thanh <n.c.thanh0801@gmail.com>
2016-12-07 13:22:27 +0700

    admin-list-product

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 gem "rails", "~> 5.0.0", ">= 5.0.0.1"
 
 gem "sqlite3"
-
+gem "kaminari"
 gem "font-awesome-rails"
 gem "i18n-js", ">= 3.0.0.rc11"
 gem "bootstrap-sass", "3.3.6"
@@ -13,7 +13,7 @@ gem "carrierwave", "0.11.2"
 gem "mini_magick", "4.5.1"
 gem "fog", "1.38.0"
 gem "devise"
-
+gem "config"
 gem "puma", "~> 3.0"
 
 gem "sass-rails", "~> 5.0"
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,12 +6,21 @@ class Product < ApplicationRecord
   has_many :favorites
   has_many :order_details
 
-  validates :content, presence: true, length: {maximum: 50},
+  paginates_per 10
+  validates :name, presence: true, length: {maximum: 50},
     uniqueness: {case_sensitive: false}
   validates :price, presence: true, length: {minimum: 10}
   validates :quantity, presence: true, length: {minimum: 6}
-
   mount_uploader :image, ImageUploader
   accepts_nested_attributes_for :specifications, allow_destroy: true,
     reject_if: proc{|attributes| attributes["feature_value"].blank?}
+  scope :in_category, -> category_id do
+    where category_id: category_id if category_id.present?
+  end
+
+  class << self
+    def list_product
+      self.order(created_at: :ASC).includes(:category)
+    end
+  end
 end
