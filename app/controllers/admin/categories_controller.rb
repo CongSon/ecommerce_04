c6e4464d110748abc6c5c93e1d4d9818a7a252ac
SonNC <sonncse03032@gmail.com>
2016-12-10 11:06:07 +0700

    admin-manage-category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,11 @@
 class Admin::CategoriesController < ApplicationController
   load_and_authorize_resource
-  before_action :verify_admin
+  before_action :authenticate_user!, :verify_admin
   layout "admin"
 
   def index
+    @search = Category.search params[:q]
+    @categories = @search.result.page params[:page]
   end
 
   def new
@@ -20,6 +22,27 @@ class Admin::CategoriesController < ApplicationController
     redirect_to :back
   end
 
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".update_success"
+    else
+      flash[:notice] = t".update_fail"
+    end
+    redirect_to :back
+  end
+
+  def destroy
+    if !@category.is_leaf?
+      flash[:notice] = t ".must_be_leaf"
+    elsif @category.products.any?
+      flash[:notice] = t ".notice_message"
+    elsif @category.destroy && @category.delete_category
+      flash[:success] = t ".delete_success"
+    else
+      flash[:notice] = t ".delete_fail"
+    end
+    redirect_to :back
+  end
   private
   def category_params
     params.permit :name, :description
