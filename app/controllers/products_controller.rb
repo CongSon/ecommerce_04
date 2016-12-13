f0b2dc9595cb4469f6954d7e18cddab8f2969bb0
SonNC <sonncse03032@gmail.com>
2016-12-13 10:15:09 +0700

    recent-view-product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,26 @@
 class ProductsController < ApplicationController
   load_and_authorize_resource
+  before_action :load_session_recent_view
 
   def index
     @hot_trend_products = Product.hot_trend
     @newest_products = Product.order(created_at: :desc)
       .limit(Settings.product.limit)
+    @recent_products = session[:recent].map{|id| Product.find_by(id: id)} if
+      session[:recent]
   end
 
   def show
+    session[:recent] = [] unless session[:recent]
+    if session[:recent].length >= Settings.max_recent
+      session[:recent].delete_at 0
+    end
+    session[:recent].push @product.id
     @specifications = @product.specifications
+  end
+
+  private
+  def load_session_recent_view
+    @cart = session[:recent] ? session[:recent] : []
   end
 end
